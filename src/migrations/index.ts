f40a8ac7e98610251a161f6f07f4dd2031c53f22
yatochka-dev <philipchef13@gmail.com>
2025-03-30 00:22:22 +0300

    add migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250329_211918 from './20250329_211918';
+import * as migration_20250329_212155 from './20250329_212155';
 
 export const migrations = [
   {
-    up: migration_20250329_211918.up,
-    down: migration_20250329_211918.down,
-    name: '20250329_211918'
+    up: migration_20250329_212155.up,
+    down: migration_20250329_212155.down,
+    name: '20250329_212155'
   },
 ];
