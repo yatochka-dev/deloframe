c3b208c14e4e3294ae9bf659950931a2d3ed2335
yatochka-dev <philipchef13@gmail.com>
2025-04-04 17:38:41 +0300

    feat: migrations refactor: run prettier

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250402_210645 from './20250402_210645';
+import * as migration_20250404_143826 from './20250404_143826';
 
 export const migrations = [
   {
     up: migration_20250402_210645.up,
     down: migration_20250402_210645.down,
-    name: '20250402_210645'
+    name: '20250402_210645',
+  },
+  {
+    up: migration_20250404_143826.up,
+    down: migration_20250404_143826.down,
+    name: '20250404_143826'
   },
 ];
